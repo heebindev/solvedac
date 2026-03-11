28fdadc5d7b10806f479ab65c72cedfe5e0c8038
HeebinMAC <heebin_mac@HeebinMACui-MacBookAir.local>
2026-03-11 15:46:28 +0900

    init: solvedac java project setup

---
+++ src/class1/Baejoon_1000.java
@@ -0,0 +1,15 @@
+package class1;
+
+import java.util.Scanner;
+
+public class Baejoon_1000 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A+B);
+    }
+}
