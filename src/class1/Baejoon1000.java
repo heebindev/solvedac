512df4dd8d749526381683f213accef1ec8afeb6
heebindev <heebin1999@naver.com>
2026-03-18 16:35:13 +0900

    boj1000

--- src/class1/Baejoon1000.java
+++ src/class1/Baejoon1000.java
@@ -2,7 +2,7 @@ package class1;
 
 import java.util.Scanner;
 
-public class Baejoon_1000 {
+public class Baejoon1000 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
