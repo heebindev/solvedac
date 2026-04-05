83d9f414ab4e633f46b4f786c58cb5e66e8b9f43
heebindev <heebin1999@naver.com>
2026-04-05 14:12:56 +0900

    10951

---
+++ src/class1/Baejoon10951.java
@@ -0,0 +1,17 @@
+package class1;
+
+import java.util.Scanner;
+
+public class Baejoon10951 {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+
+        while (sn.hasNextInt()){
+
+            int A = sn.nextInt();
+            int B = sn.nextInt();
+
+            System.out.println(A+B);
+        }
+    }
+}
