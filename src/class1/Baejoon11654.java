716efc331b9e616c1171c0163ddf034b6b87c799
heebindev <heebin1999@naver.com>
2026-04-05 14:41:34 +0900

    11654

---
+++ src/class1/Baejoon11654.java
@@ -0,0 +1,13 @@
+package class1;
+
+import java.util.Scanner;
+
+public class Baejoon11654 {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+
+        char A = sn.next().charAt(0);
+
+        System.out.println((int)A);
+    }
+}
